63ac0520bdfbc2f798e5ee6298b151905c92baa4
Zenderg <danil54658798@gmail.com>
2026-02-01 21:19:53 +0300

    try get user data from telegram

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,6 +5,10 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app/app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { init } from '@tma.js/sdk';
+
+// Initialize Telegram Mini Apps SDK before bootstrapping the app
+init();
 
 bootstrapApplication(AppComponent, {
   providers: [provideHttpClient(), provideRouter(routes), provideAnimations()],
